34ec92c3f21a5c136c131d15e276567d586dcc36
abhin <abhin.pik@gmail.com>
2024-10-26 15:26:11 +0100

    node js http example with fs

--- Netfilx UI with Next Js/index.js
+++ Netfilx UI with Next Js/index.js
@@ -2,16 +2,18 @@ import http from "http";
 import fs from "fs";
 
 function handleRequest(req, res) {
-    console.log(req.url)
-  fs.readFile(
-    `NetflixUI/${req.url == "/" ? "index.html" : req.url}`,
-    (err, data) => {
-      if (err) {
-        res.write("404 Internal error");
-      } else res.write(data);
+  console.log(req.url);
+  const path = `NetflixUI${req.url == "/" ? "/index.html" : req.url}`;
+  if (fs.existsSync(path)) {
+    fs.readFile(path, (err, data) => {
+      console.log(err);
+      (err && res.write("500 Internal error")) || res.write(data);
       res.end();
-    }
-  );
+    });
+  } else {
+    res.write("404 file not found");
+    res.end();
+  }
 }
 
 const server = http.createServer(handleRequest);
